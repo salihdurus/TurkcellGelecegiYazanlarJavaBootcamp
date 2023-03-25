23ed02e11e07b07133ab1ac80056df19485ad0ef
salihdurus <durussalih@gmail.com>
2023-03-25 12:48:05 +0300

    CRUD

---
+++ Homework - ECommerce/src/main/java/com/gy/ecommerce/entities/Product.java
@@ -0,0 +1,24 @@
+package com.gy.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double unitPrice;
+    private int quantity;
+    private String description;
+
+}
